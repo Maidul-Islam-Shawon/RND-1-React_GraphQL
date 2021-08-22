4ccd02a5536b9ee4ddd817f55b3732efacaf98da
Maidul-Islam <maidul.bd@asa-international.com>
2021-08-22 22:33:37 +0600

    Fragment included to get common fields.

--- react_graphql/src/GraphQL/Queries.js
+++ react_graphql/src/GraphQL/Queries.js
@@ -1,32 +1,51 @@
+//................. Customer Queries...............//
+
+//.....Fragment for common fields.....//
+const CUSTOMER_FIELDS = `
+  fragment CustomerFields on Customer{
+    name age email contactNumber address
+  }
+`;
+
+//.....query to get all Customer.....//
 export const GET_ALL_CUSTOMERS = `
     query GetAllCustomers{
         allCustomers{
-        id name age email contactNumber address
+        id, 
+        ...CustomerFields
         }
-    }
+    }${CUSTOMER_FIELDS}
 `;
 
+//.....query to Create Customer.....//
 export const CREATE_CUSTOMER = `
   mutation CreateCustomer($Customer:CustomerInput!){
     createCustomer(customer:$Customer){
-      id name age email contactNumber address
+      id, 
+      ...CustomerFields
     }
-}`;
+}${CUSTOMER_FIELDS}
+`;
 
+//.....query to Update Customer.....//
 export const UPDATE_CUSTOMER = `
   mutation UpdateCustomer($Id:ID!,$CustomerData:CustomerUpdate){
     updateCustomer(id:$Id,customer:$CustomerData){
-      name age address email
+      ...CustomerFields
     }
-}`
+}${CUSTOMER_FIELDS}
+`;
 
+//.....query to get Customer by Id.....//
 export const GET_CUSTOMER_BY_ID = `
     query GetCustomerById($Id:ID!){
     customerByid(id:$Id){
-      name age address email contactNumber
+      ...CustomerFields
     }
-  }`
+  }${CUSTOMER_FIELDS}
+  `;
 
+//.....query to Delete Customer.....//
 export const DELETE_CUSTOMER = `
     mutation DeleteCustomer($Id:ID!){
         deleteCustomer(id:$Id)
