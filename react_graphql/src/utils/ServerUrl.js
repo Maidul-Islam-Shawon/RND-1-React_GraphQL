7c9064b4615bed1b07ec4ddcf0870cc750199013
Maidul-Islam <maidul.bd@asa-international.com>
2021-09-07 02:47:09 +0600

    Kubernetes configured with deployment,service and pods.

--- react_graphql/src/utils/ServerUrl.js
+++ react_graphql/src/utils/ServerUrl.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "https://localhost:44371/graphql";
+//export const SERVER_URL = "https://localhost:44371/graphql";
+export const SERVER_URL = "http://localhost:5005/graphql";
